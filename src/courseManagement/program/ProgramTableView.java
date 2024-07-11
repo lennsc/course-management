5bc1d1146e99b099e3e9da3b02522bc4e96b6ad6
Alina <alina.unruh@edu.fhdw.de>
2024-07-11 14:27:49 +0200

    Add constructor and methods to ProgramTableView

--- src/courseManagement/program/ProgramTableView.java
+++ src/courseManagement/program/ProgramTableView.java
@@ -1,5 +1,27 @@
 package courseManagement.program;
 
-public class ProgramTableView {
+import java.util.List;
 
+import courseManagement.Program;
+import courseManagement.utils.TableView;
+
+public class ProgramTableView extends TableView<Program> {
+
+	protected ProgramTableView() {
+		super(Program.class, "Programme", List.of("Name"));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onClickRowAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void updateTable(List<Program> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
